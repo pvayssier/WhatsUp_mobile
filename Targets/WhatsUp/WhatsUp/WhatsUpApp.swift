976e9f14a7fdaf57a84ca7ca2d5db20fa91d1a87
pvayssier <paul.vayssier@ynov.com>
2024-04-08 23:02:07 +0200

    init project with great archi

---
+++ Targets/WhatsUp/WhatsUp/WhatsUpApp.swift
@@ -0,0 +1,18 @@
+//
+//  WhatsUpApp.swift
+//  WhatsUp
+//
+//  Created by Paul VAYSSIER on 08/04/2024.
+//
+
+import SwiftUI
+import Authentification
+
+@main
+struct WhatsUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Register()
+        }
+    }
+}
